8dd83c56c5dbc6117fe8a6b0319f27ef94d06f12
antinutino <maharabopi73@gmail.com>
2024-12-19 05:52:41 +0600

    debuging

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from '../components/header'
-import Body1 from '../components/Body1'
-import FoodCard from './Menu'
 import { Outlet } from 'react-router-dom'
 
 function Home() { 
