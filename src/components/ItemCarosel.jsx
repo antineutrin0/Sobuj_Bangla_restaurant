ac7788b793a90c288e83a52c7c9af5be58831279
antinutino <maharabopi73@gmail.com>
2024-12-19 20:54:59 +0600

    ses hoeo hoilo na sesh

--- src/components/ItemCarosel.jsx
+++ src/components/ItemCarosel.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import foodData from './foodData.json';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../appwrite/AuthConfig';
 
 const ItemCarosel = () => {
+  const {user}=useAuth();
   const navigate=useNavigate();
   return (
     <div className='flex flex-col md:flex-row items-center justify-between'>
-      <div className="carousel carousel-end bg-stone-950 rounded-box mx-4 w-2/3 space-x-4 p-4">
+      <div className=' border md:hidden p-2 rounded-lg my-6 lg:my-8'>
+        <p className='backdrop-blur-md text-amber-600 text-center text-xl md:text-2xl  font-semibold py-2 '>--POPULAR ITEMS--</p>
+      </div>
+      <div className="carousel carousel-end bg-stone-950 rounded-box mx-4 w-full md:w-2/3 space-x-4 p-4">
       {foodData.map((item) => (
         <div key={item.id} className="carousel-item w-full md:w-1/2 relative bg-zinc-900 rounded-xl transition-transform transform hover:scale-105 ">
           <img
@@ -21,16 +26,19 @@ const ItemCarosel = () => {
       ))}
     </div>
     <div className='flex flex-col items-center justify-center mx-auto'>
-    <div className=' border p-2 rounded-lg my-6 lg:my-8'>
+    <div className=' border hidden md:block p-2 rounded-lg my-6 lg:my-8'>
         <p className='backdrop-blur-md text-amber-600 text-center text-xl md:text-2xl  font-semibold py-2 '>--POPULAR ITEMS--</p>
       </div>
       <div>
-      <p className=" text-lg font-thin mt-2 rounded-lg p-2">This Restaurant offers an authentic taste of Bengal,<br /> blending traditional flavors with modern charm. <br /> With every dish crafted to perfection, <br /> it’s a place where great food meets heartfelt hospitality.</p>
+      <p className=" text-lg hidden md:block font-thin mt-2 rounded-lg p-2">This Restaurant offers an authentic taste of Bengal,<br /> blending traditional flavors with modern charm. <br /> With every dish crafted to perfection, <br /> it’s a place where great food meets heartfelt hospitality.</p>
       </div>
       <div>
         <button className='text-xl bg-green-500 rounded-lg p-2  text-black font-semibold hover:bg-green-600'
         onClick={()=>{
+          if(user)
           navigate('/dashboard/customer/orderfood')
+        else
+        navigate('/signin');
         }}
         >Order Here</button>
       </div>
